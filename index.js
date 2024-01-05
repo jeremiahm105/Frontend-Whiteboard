a4ee1a662247810fb6df0ebec48977addfee7936
Jeremiah Matthews <jeremiah.matthews105@gmail.com>
2024-01-05 02:58:44 -0600

    working on problems

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function reverseString (str) {
 reverseString("hello")
 console.log(reverseString("hello"));
 
+//TODO: Write a program that prints the numbers from 1-100. For multiples of three print Fizz, for multiples of 5 print Buzz for multiples of 3 and 5 print FizzBuzz.
+
+for (let i = 1; i <= 15; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
